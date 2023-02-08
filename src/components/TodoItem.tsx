468eb57ff259f8ad6eab14e693ea1fe594384c62
Robert Maksymow <114484269+RobertMaksymow@users.noreply.github.com>
2023-02-08 11:59:36 +0000

    Frontend works, needs priority to be added, styling and new backend from scratch

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 import { FiEdit } from "react-icons/fi";
 import { AiOutlineDelete } from "react-icons/ai";
@@ -7,6 +7,7 @@ import { MdOutlineDone, MdOutlineRemoveDone } from "react-icons/md";
 import "./TodoItem.css";
 
 import { Todo } from "../model";
+import TodoList from "./TodoList";
 
 interface Props {
   todo: Todo;
@@ -15,30 +16,81 @@ interface Props {
 }
 
 const TodoItem = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  };
+
+  const handleEdit = (event: React.FormEvent, id: number) => {
+    event.preventDefault(); //Prevents screen being refreshed
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
-    <form className="todos__single">
-      <span className="todos__single--text">
-        {todo.id}: {todo.todo}
-        <div>
-          <span className="icon">
-            {" "}
-            <FiEdit />
-            edit
-          </span>
-          <span className="icon">
-            <AiOutlineDelete />
-            delete
-          </span>
-          <span className="icon">
-            {todo.isCompleted === true ? (
-              <MdOutlineRemoveDone />
-            ) : (
-              <MdOutlineDone />
-            )}
-            done
-          </span>
-        </div>
-      </span>
+    <form
+      className="todos__single"
+      onSubmit={(event) => handleEdit(event, todo.id)}
+    >
+      {edit ? (
+        <input
+          className="todos__single--text"
+          value={editTodo}
+          ref={inputRef}
+          onChange={(event) => setEditTodo(event.target.value)}
+        />
+      ) : todo.isCompleted ? (
+        <s className="todos__single--text">
+          {todo.id}: {todo.todo}
+        </s>
+      ) : (
+        <span className="todos__single--text">
+          {todo.id}: {todo.todo}
+        </span>
+      )}
+
+      <div>
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isCompleted) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <FiEdit />
+          edit
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiOutlineDelete />
+          delete
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          {todo.isCompleted === true ? (
+            <MdOutlineRemoveDone />
+          ) : (
+            <MdOutlineDone />
+          )}
+          done
+        </span>
+      </div>
     </form>
   );
 };
